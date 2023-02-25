b545eb77fcc3b4b33626de35d0f84ce4c048e561
Richard Washington <richard.washington@taxassist.co.uk>
2023-02-25 19:47:53 +0000

    :sparkles: Ships can be hit enough to be sunk

--- ship.js
+++ ship.js
@@ -1,11 +1,35 @@
 export default class Ship {
     #size;
+    #_sunk;
+    #hits;
 
     constructor(size) {
         this.#size = size;
+        this.#_sunk = false;
+        this.#hits = 0;
     }
 
     get size() {
         return this.#size;
+    }
+
+    get isSunk() {
+        return this.#_sunk;
+    }
+
+    set #sunk(sunk) {
+        this.#_sunk = sunk;
+    }
+
+    hit() {
+        this.#hits++;
+
+        if (this.#hits === this.#size) {
+            this.#sunk = true;
+        }
+    }
+
+    get hits() {
+        return this.#hits;
     }
 }
--- ship.test.js
+++ ship.test.js
@@ -11,3 +11,28 @@ test('The size of a ship cannot be changed after it has been created', () => {
         ship.size = 6
     ).toThrow();
 });
+
+test('Ships are initialised with 0 hits', () => {
+    expect(ship.hits).toBe(0);
+});
+
+test('Ships are initialised as not sunk', () => {
+    expect(ship.isSunk).toBe(false);
+});
+
+test('Ships that are hit have hits', () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+});
+
+test('Ships that have not been hit enough are not sunk', () => {
+    expect(ship.isSunk).toBe(false);
+});
+
+test('Ships that get hit enough become sunk', () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk).toBe(true);
+});
