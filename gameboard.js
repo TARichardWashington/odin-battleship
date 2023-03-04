776796cc50288f2c6a3e46edbd5e9bb997194ae3
Richard Washington <richard.washington@taxassist.co.uk>
2023-03-04 17:26:09 +0000

    :sparkles: Enable adding of ships to the game board

--- gameboard.js
+++ gameboard.js
@@ -1,5 +1,43 @@
-export default class Gameboard {
-    constructor() {
+import Ship from './ship.js';
 
+export default class Gameboard {
+    #coordinates;
+    #ships;
+
+    constructor() {
+        this.#coordinates =
+            [
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null],
+
+            ];
+
+        this.#ships = [];
+    }
+
+    placeShip(x, y, direction, size) {
+        let ship = new Ship(size);
+
+        this.#ships.push([ship, direction, x, y]);
+
+        for (let i = 0; i < size; i++) {
+            this.#coordinates[x + i][y] = 1;
+        }
+    }
+
+    get ships() {
+        return this.#ships;
+    }
+
+    shipAt(x, y) {
+        return this.#coordinates[x][y];
     }
 }
--- gameboard.test.js
+++ gameboard.test.js
@@ -1,7 +1,32 @@
 import Gameboard from './gameboard.js';
+import Ship from './ship.js';
 
 const gameboard = new Gameboard;
 
 test('Gamebaord can be instantiated', () => {
     expect(gameboard).toBeDefined();
+});
+
+test('Ships can be added to the gameboard', () => {
+    gameboard.placeShip(0, 0, true, 3);
+    let ships = gameboard.ships;
+
+    expect(Array.isArray(ships)).toBe(true);
+    expect(ships[0][0] instanceof Ship).toBe(true);
+    expect(ships[0][1]).toBe(true);
+    expect(ships[0][2]).toBe(0);
+    expect(ships[0][3]).toBe(0);
+    expect(gameboard.shipAt(0, 0)).toBe(1);
+});
+
+test('Ships can be added to the gameboard at a different location', () => {
+    gameboard.placeShip(5, 5, true, 3);
+    let ships = gameboard.ships;
+
+    expect(Array.isArray(ships)).toBe(true);
+    expect(ships[1][0] instanceof Ship).toBe(true);
+    expect(ships[1][1]).toBe(true);
+    expect(ships[1][2]).toBe(5);
+    expect(ships[1][3]).toBe(5);
+    expect(gameboard.shipAt(5, 5)).toBe(1);
 });
